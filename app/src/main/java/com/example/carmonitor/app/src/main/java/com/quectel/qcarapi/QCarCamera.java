c3fa208181a25c626314778ffcd1553a314ea2fe
Ch0327 <chrdw0327@gmail.com>
2026-09-11 01:04:41 +0800

    Update QCarCamera.java

--- app/src/main/java/com/example/carmonitor/app/src/main/java/com/quectel/qcarapi/QCarCamera.java
+++ app/src/main/java/com/example/carmonitor/app/src/main/java/com/quectel/qcarapi/QCarCamera.java
@@ -1,16 +1,1 @@
-package com.quectel.qcarapi;
 
-import android.view.Surface;
-
-public class QCarCamera {
-    static {
-        System.loadLibrary("mmqcar_qcar_jni");
-    }
-
-    public native int cameraOpen(int csi, int channel, int flag);
-    public native int cameraClose(int csi, int channel);
-    public native int setVideoSize(int csi, int channel, int width, int height);
-    public native int setFps(int csi, int channel, int fps);
-    public native int startPreview(int csi, int channel, Surface surface);
-    public native int stopPreview(int csi, int channel);
-}
